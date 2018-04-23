01347676828c48a893b706967bec574c20f62b06
Hillel Cohen <hillelcohen004@gmail.com>
2018-04-23 10:21:23 +0300

    add build shell script (#2)

--- README.md
+++ README.md
@@ -1,22 +1,17 @@
 # steps to run server on linux:
 
-1) cd GuruxDLMS/development
+1) ./build_server.sh
 
-2) mkdir obj lib
-
-3) make
-
-4) cd ../../
-
-5) mkdir obj bin
-
-6) make
-
-7) ./bin/server.bin <flag> <argument>
+2) ./bin/server.bin <flag> <argument>
 
 configurations options:
 interface:         -i         wrapper/hdlc     (default = wrapper)
 protocol:          -protocol  udp/tcp          (default = udp)
 port:              -p         <number>         (default = 4061)
 max pdu size:      -m         <number>         (default = 1024)
-conformance block: -c         <number in hex>  (default is as requested by Yael) 
+conformance block: -c         <number in hex>  (default is as requested by Yael)
+
+
+# clean:
+
+1) ./build_server.sh -c
---
+++ build_server.sh
@@ -0,0 +1,41 @@
+#----------------------------------------------------------------------------
+#   The confidential and proprietary information contained in this file may
+#   only be used by a person authorised under and to the extent permitted
+#   by a subsisting licensing agreement from ARM Limited or its affiliates.
+#
+#          (C) COPYRIGHT 2013-2016 ARM Limited or its affiliates.
+#              ALL RIGHTS RESERVED
+#
+#   This entire notice must be reproduced on all copies of this file
+#   and copies of this file may only be made by a person if such person is
+#   permitted to do so under the terms of a subsisting license agreement
+#   from ARM Limited or its affiliates.
+#----------------------------------------------------------------------------
+# source (!) this file while at tree top - DO NOT run it
+
+CLEAN="$1"
+
+if [[ "$CLEAN" == "-c" ]]; then
+	rm -rf GuruxDLMS/development/obj GuruxDLMS/development/lib ./obj ./bin
+else
+	cd GuruxDLMS/development
+
+	if [ ! -d "obj" ]; then
+	mkdir obj
+	fi
+	if [ ! -d "lib" ]; then
+	mkdir lib
+	fi
+
+	make -j10
+	cd ../../
+
+	if [ ! -d "obj" ]; then
+	mkdir obj
+	fi
+	if [ ! -d "bin" ]; then
+	mkdir bin
+	fi
+
+	make -j10
+fi
